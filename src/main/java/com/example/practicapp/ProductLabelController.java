a242e4ea35e267a773739a080692b826f0221dc6
Laptop <mmamano@inbox.ru>
2021-11-28 15:36:22 +0300

    Created product label and product controller

---
+++ src/main/java/com/example/practicapp/ProductLabelController.java
@@ -0,0 +1,4 @@
+package com.example.practicapp;
+
+public class ProductLabelController {
+}
--- src/main/java/com/example/practicapp/ProductListController.java
+++ src/main/java/com/example/practicapp/ProductListController.java
@@ -3,9 +3,14 @@ package com.example.practicapp;
 import com.example.practicapp.MainMenuController;
 import com.example.practicapp.objects.Product;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 
+import java.io.IOException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
@@ -23,9 +28,56 @@ public class ProductListController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        updateGrid("");
+    }
+    public void updateGrid(String filter){
+
+        gridPane.getChildren().clear();
         List<Product> products = DatabaseController.getProducts();
-        for (int i = 0; i < products.size(); i++) {
-            
+        int column = 0;
+        int row = 1;
+        try {
+            for (int i = 0; i < products.size(); i++) {
+                if (products.get(i).getName().contains(filter)) {
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("ProductLabel.fxml"));
+                    Pane anchorPane = fxmlLoader.load();
+
+                    ProductLabelController controller = fxmlLoader.getController();
+                    //controller.setData(PhotographList.get(i).getPhotograph(), PhotographList.get(i).getDescription(), menuController);
+                    if (column == 2) {
+                        column = 0;
+                        row++;
+                    }
+                    //
+                    gridPane.add(anchorPane, column++, row);
+                    GridPane.setMargin(anchorPane, new Insets(5));
+                }
+                else if (filter == ""){
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("ProductLabel.fxml"));
+                    Pane anchorPane = fxmlLoader.load();
+
+                    ProductLabelController controller = fxmlLoader.getController();
+                   // controller.setData(PhotographList.get(i).getPhotograph(), PhotographList.get(i).getDescription(), menuController);
+                    if (column == 2) {
+                        column = 0;
+                        row++;
+                    }
+                    //
+                    gridPane.add(anchorPane, column++, row);
+                    GridPane.setMargin(anchorPane, new Insets(5));
+                }
+                gridPane.setMinWidth(Region.USE_COMPUTED_SIZE);
+                gridPane.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                gridPane.setMaxWidth(Region.USE_COMPUTED_SIZE);
+                //
+                gridPane.setMinHeight(Region.USE_COMPUTED_SIZE);
+                gridPane.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                gridPane.setMaxHeight(Region.USE_COMPUTED_SIZE);
+            }
+        } catch(IOException e){
+            e.printStackTrace();
         }
     }
 }
